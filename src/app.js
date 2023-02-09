17c63b1d0bd1868673a9ce5cd688b7355051de9a
marcoscutaro <marcostruggia@gmail.com>
2023-02-09 11:36:39 +0000

    changesitos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const generate = document.getElementById("generate");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+generate.addEventListener("click", () => {
+  document.querySelector(".excuse").innerHTML = generateExcuse();
+});
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let last = [".com"];
+
+  let outcomes = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++)
+        for (let o = 0; o < last.length; o++) {
+          outcomes.push(pronoun[i] + adj[j] + noun[k] + last[o] + "<br>");
+        }
+    }
+  }
+  return outcomes;
 };
